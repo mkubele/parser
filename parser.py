44c9975c537d5945e012977b6b009b4ea9331b5d
Michal Kubele <michal.kubele@gmail.com>
2017-12-09 12:34:28 +0100

    edit

--- parser.py
+++ parser.py
@@ -8,12 +8,10 @@ cont = None
 
 def parser():
     if file.lower().endswith('.txt'):
-
         if arg == '-s':
             start = None
             end = None
             format = "%m-%d %H:%M:%S.%f"
-
             with open(file, 'r') as fl:
                 for line in fl:
                     if 'TEST STARTED' in line:
@@ -26,8 +24,7 @@ def parser():
                     dur = end - start
                 else:
                     sys.exit('no test run found')
-                print(dur)
-
+                print('Difference between “TEST STARTED” and “TEST FINISHED is', dur)
         elif arg == '-i':
             cont = sys.argv[3]
             cont = cont.split(',')
@@ -36,28 +33,22 @@ def parser():
                 for line in fl:
                     if all(value in line for value in cont):
                         print(line)
-
         elif arg == '-e':
             cont = sys.argv[3]
             cont = cont.split(',')
-
             with open(file, 'r') as fl:
                 for line in fl:
                     if not any(value in line for value in cont):
                         print(line)
-
-        elif arg == '-h':
+        elif arg == '-h' or (file == '-h' and arg is None):
             print(
                 '-h  prints out help containing info about all available switches\n\
 -s prints out the time difference between lines containing “TEST STARTED” and “TEST FINISHED”\n\
 -i <args,…> prints out lines containing all arguments\n\
 -e <args,…> prints out all lines which don\'t contain any of provided arguments')
             sys.exit(0)
-
         else:
             sys.exit('unknown argument')
-
     else:
             sys.exit('not a txt file')
-
 parser()
